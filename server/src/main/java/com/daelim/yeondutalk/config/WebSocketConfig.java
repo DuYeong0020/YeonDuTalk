b39f5fb6d1ed4053cd551a19faf7db68e7daa842
DuYeong0020 <chlendud00@naver.com>
2021-11-14 18:09:07 +0900

    fix: CORS 처리

--- server/src/main/java/com/daelim/yeondutalk/config/WebSocketConfig.java
+++ server/src/main/java/com/daelim/yeondutalk/config/WebSocketConfig.java
@@ -4,14 +4,21 @@ import com.daelim.yeondutalk.repository.ChatWebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer {
 
     private final static String CHAT_ENDPOINT = "/chat";
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(getChatWebSocketHandler(), CHAT_ENDPOINT)
