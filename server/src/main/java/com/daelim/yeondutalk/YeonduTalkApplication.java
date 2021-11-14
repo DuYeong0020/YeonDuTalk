bbbe149046cb2f23b0d6f8bba9dbdfd2c0b9f5f1
DuYeong0020 <chlendud00@naver.com>
2021-11-14 13:37:53 +0900

    init: 서버 초기화

---
+++ server/src/main/java/com/daelim/yeondutalk/YeonduTalkApplication.java
@@ -0,0 +1,13 @@
+package com.daelim.yeondutalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeonduTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeonduTalkApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/daelim/yeondutalk/YeonduTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daelim.yeondutalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeonduTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
